63032d392702231546c4d1d34c998168b4308955
Sam de Bruyn <sam@polarmonitoring.com>
2024-08-20 00:19:28 +0200

    Add MatchingEngine.java file and update package-info for com.ordrebook.service

--- AlgorithmicTradingAssessment/src/com/orderbook/service/package-info.java
+++ AlgorithmicTradingAssessment/src/com/orderbook/service/package-info.java
@@ -1,8 +1,10 @@
 /**
- * This package contains the necessary services for managing the order book
+ * This package contains the necessary services for managing the order book and the matching engine
  * <p>
- * It includes the methods that handle the order book logic of adding, modifying, and removing orders:
+ * It includes the methods that handle the order book logic of adding, modifying, and removing orders.
+ * This package also includes the methods required for processing and matching orders (matchingEngine).
  * - {@link com.orderbook.service.OrderBook} is the main class that manages the order book and handles the operations on orders.
+ * - {@link com.orderbook.service.MatchingEngine} is the class that manages the matching engine and handles order processing and matching.
  * </p>
  */
 package com.orderbook.service;
